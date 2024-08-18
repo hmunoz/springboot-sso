07c99c15b12b3facf2bcf57060d9ec84022b6c1d
UNRN_HC/hmunoz <hmunoz@unrn.edu.ar>
2024-08-18 20:03:02 -0300

    fix SwaggerConfig.java

--- src/main/java/ar/unrn/video/config/OpenApi30Config.java
+++ src/main/java/ar/unrn/video/config/OpenApi30Config.java
@@ -1,16 +1,53 @@
 package ar.unrn.video.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.OAuthFlow;
-import io.swagger.v3.oas.annotations.security.OAuthFlows;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.*;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Videoclub", version = "v1"))
-@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.OAUTH2,
-        flows = @OAuthFlows(implicit = @OAuthFlow(authorizationUrl = "${springdoc.oauth2.authorization-url}") ))
 public class OpenApi30Config {
+
+    @Value("${springdoc.oauth2.authorization-url}")
+    private String authorizationUrl;
+
+    @Bean
+    public OpenAPI openApiSpec() {
+        return new OpenAPI().components(new Components()
+                .addSchemas("ApiErrorResponse", new ObjectSchema()
+                        .addProperty("status", new IntegerSchema())
+                        .addProperty("code", new StringSchema())
+                        .addProperty("message", new StringSchema())
+                        .addProperty("fieldErrors", new ArraySchema().items(
+                                new Schema<ArraySchema>().$ref("ApiFieldError"))))
+                .addSchemas("ApiFieldError", new ObjectSchema()
+                        .addProperty("code", new StringSchema())
+                        .addProperty("message", new StringSchema())
+                        .addProperty("property", new StringSchema())
+                        .addProperty("rejectedValue", new ObjectSchema())
+                        .addProperty("path", new StringSchema()))
+                // bearerAuth  keycloak
+                .addSecuritySchemes("bearerAuth", new io.swagger.v3.oas.models.security.SecurityScheme()
+                        .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.OAUTH2)
+                        .flows(new io.swagger.v3.oas.models.security.OAuthFlows()
+                                .implicit(new io.swagger.v3.oas.models.security.OAuthFlow()
+                                        .authorizationUrl(authorizationUrl)))));
+    }
+
+    @Bean
+    public OperationCustomizer operationCustomizer() {
+        // add error type to each operation
+        return (operation, handlerMethod) -> {
+            operation.getResponses().addApiResponse("4xx/5xx", new ApiResponse()
+                    .description("Error")
+                    .content(new Content().addMediaType("*/*", new MediaType().schema(
+                            new Schema<MediaType>().$ref("ApiErrorResponse")))));
+            return operation;
+        };
+    }
 }
