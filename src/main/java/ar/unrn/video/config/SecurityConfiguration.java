d7b4d068a72841036ffe94f873f00869fa2756d8
UNRN_HC/hmunoz <hmunoz@unrn.edu.ar>
2024-08-19 20:13:16 -0300

    add http client

--- src/main/java/ar/unrn/video/config/SecurityConfiguration.java
+++ src/main/java/ar/unrn/video/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -24,7 +25,7 @@ class SecurityConfiguration {
 
         http
                 .cors(withDefaults())
-                .csrf(csrf -> csrf.disable());
+                .csrf(AbstractHttpConfigurer::disable);
 
         http.sessionManagement(sessionManagement ->
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
--- src/main/java/ar/unrn/video/rest/TestResource.java
+++ src/main/java/ar/unrn/video/rest/TestResource.java
@@ -35,24 +35,27 @@ public class TestResource {
 
     @GetMapping
     @Operation(summary = "get all Test", security = @SecurityRequirement(name = "bearerAuth"))
-    @PreAuthorize("hasRole('admin')")
     public ResponseEntity<List<TestDTO>> getAllTests() {
         return ResponseEntity.ok(testService.findAll());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "get  Test", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<TestDTO> getTest(@PathVariable(name = "id") final Long id) {
         return ResponseEntity.ok(testService.get(id));
     }
 
     @PostMapping
     @ApiResponse(responseCode = "201")
+    @Operation(summary = "create  Test", security = @SecurityRequirement(name = "bearerAuth"))
+    @PreAuthorize("hasRole('admin')")
     public ResponseEntity<Long> createTest(@RequestBody @Valid final TestDTO testDTO) {
         final Long createdId = testService.create(testDTO);
         return new ResponseEntity<>(createdId, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "update  Test", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<Long> updateTest(@PathVariable(name = "id") final Long id,
             @RequestBody @Valid final TestDTO testDTO) {
         testService.update(id, testDTO);
@@ -61,6 +64,8 @@ public class TestResource {
 
     @DeleteMapping("/{id}")
     @ApiResponse(responseCode = "204")
+    @PreAuthorize("hasRole('admin')")
+    @Operation(summary = "delete by id", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<Void> deleteTest(@PathVariable(name = "id") final Long id) {
         testService.delete(id);
         return ResponseEntity.noContent().build();
