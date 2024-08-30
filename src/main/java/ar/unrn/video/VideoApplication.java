7d2bc245ce1930a7242658f40731387d204dcc42
UNRN_HC/hmunoz <hmunoz@unrn.edu.ar>
2024-08-29 21:41:51 -0300

    add  api gateway

--- src/main/java/ar/unrn/video/VideoApplication.java
+++ src/main/java/ar/unrn/video/VideoApplication.java
@@ -2,6 +2,9 @@ package ar.unrn.video;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -9,6 +12,17 @@ public class VideoApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(VideoApplication.class, args);
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("http://localhost:5173").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+            }
+        };
     }
 
 }
--- src/main/java/ar/unrn/video/rest/TestResource.java
+++ src/main/java/ar/unrn/video/rest/TestResource.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/tests", produces = MediaType.APPLICATION_JSON_VALUE)
-@CrossOrigin(origins = "*")
 public class TestResource {
 
     private final TestService testService;
